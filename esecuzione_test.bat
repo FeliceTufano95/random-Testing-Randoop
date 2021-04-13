3c08d306930a4858255b04e82c9840eedbb64860
FeliceTufano95 <80173344+FeliceTufano95@users.noreply.github.com>
2021-04-13 18:25:07 +0200

    Update esecuzione_test.bat

--- esecuzione_test.bat
+++ esecuzione_test.bat
@@ -4,7 +4,7 @@ REM numero del ciclo corrente
 set cycle=%1
 
 
-javac -cp ..\external_jars\junit-4.13.2.jar;. ErrorTest.java
-java -cp ..\external_jars\junit-4.13.2.jar;..\external_jars\hamcrest-core-1.3.jar;. org.junit.runner.JUnitCore ErrorTest >> EsecuzioneTestCiclo%cycle%.txt
+javac -cp ..\..\external_jars\junit-4.13.2.jar;. ErrorTest.java
+java -cp ..\..\external_jars\junit-4.13.2.jar;..\..\external_jars\hamcrest-core-1.3.jar;. org.junit.runner.JUnitCore ErrorTest >> EsecuzioneTestCiclo%cycle%.txt
 
 exit
